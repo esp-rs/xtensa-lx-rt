95b3a0506fd01e2d80d586f8e77296bd65363e87
Scott Mabin <scott@mabez.dev>
2019-08-29 16:44:31 +0100

    Blinky working (LED) but the nop delays are a bit funky

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+#![feature(asm)]
+
 use xtensa_lx6_rt as _;
 
 use core::panic::PanicInfo;
@@ -15,15 +17,9 @@ const GPIO_OUT_W1TS_REG: u32 = 0x3FF44008;
 const GPIO_OUT_W1TC_REG : u32 = 0x3FF4400C;
 
 
-
-// const GPIO_ENABLE_REG: u32 = 0x3FF44020;
-
-// const GPIO_OUT_REG: u32 = 0x3FF44004;
-
-
 const BLINKY_GPIO: u32 = 2; // the GPIO hooked up to the onboard LED
 const GPIO_FUNCX_OUT_BASE: u32 = 0x3FF44530;
-const GPIO_FUNC2_OUT_SEL_CFG: u32 = GPIO_FUNCX_OUT_BASE + (BLINKY_GPIO * 4);
+const GPIO_FUNCX_OUT_SEL_CFG: u32 = GPIO_FUNCX_OUT_BASE + (BLINKY_GPIO * 4);
 
 const IO_MUX_GPIO2_REG: u32 = 0x3FF49040;
 
@@ -40,23 +36,27 @@ fn main() -> ! {
     // set the pin as an output
     unsafe {
         core::ptr::write_volatile(GPIO_ENABLE_W1TS_REG as *mut _, 0x1 << BLINKY_GPIO);
-        core::ptr::write_volatile(GPIO_FUNC2_OUT_SEL_CFG as *mut _, 0x100); // 0x100 makes this pin a simple gpio pin - see the technical reference
+        core::ptr::write_volatile(GPIO_FUNCX_OUT_SEL_CFG as *mut _, 0x100); // 0x100 makes this pin a simple gpio pin - see the technical reference
         
         // TODO select function and drive strength
-        // let iomux_gpio2 = 0;
+        // if your led pin doesn't default to GPIO you will need to set this register see technical ref for details
         // core::ptr::write_volatile(IO_MUX_GPIO2_REG as *mut _, BLINKY_GPIO); // GPIO2 function 1 is being a gpio port
     }
     loop {
         unsafe {
-            core::ptr::write_volatile(GPIO_OUT_W1TS_REG as *mut _, 0x1 << BLINKY_GPIO);
-            let dummy_var: u32 = 0;
-            for _ in 0..32_00 {
-                unsafe { core::ptr::read_volatile(&dummy_var) };
-            }
-            // core::ptr::write_volatile(GPIO_OUT_W1TC_REG as *mut _, 0x1 << BLINKY_GPIO);
-            // for _ in 0..32_00 {
-            //     unsafe { core::ptr::read_volatile(&dummy_var) };
-            // }
+            core::ptr::write_volatile(GPIO_OUT_W1TS_REG as *mut _, 0x1 << BLINKY_GPIO);           
         }
+        delay(80_000_000);
+        unsafe {
+            core::ptr::write_volatile(GPIO_OUT_W1TC_REG as *mut _, 0x1 << BLINKY_GPIO);
+        }
+        delay(80_000_000);
+    }
+}
+
+pub fn delay(clocks: u32) {
+    let dummy_var: u32 = 0;
+    for _ in 0..clocks {
+        unsafe { core::ptr::read_volatile(&dummy_var) };
     }
 }
