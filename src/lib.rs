306fad553d5b33633acc56d09b9eb6496957db0d
Scott Mabin <scott@mabez.dev>
2020-06-06 23:48:50 +0100

    Remove non 'rt' related code, this has now been moved to xtensa-lx6

--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,6 @@ pub use r0::zero_bss;
 
 use xtensa_lx6_rt_proc_macros as proc_macros;
 
-pub mod exception;
-pub mod interrupt;
-
-#[macro_use]
-mod macros;
-
 #[doc(hidden)]
 #[no_mangle]
 pub unsafe extern "C" fn DefaultPreInit() {}
@@ -51,100 +45,30 @@ pub unsafe extern "C" fn Reset() -> ! {
 
     // Copy of data segment is done by bootloader
 
-    interrupt::disable();
-
+    set_mask(!0); // disable all interrupts
     let vecbase = &_init_start as *const u32;
-    set_vecbase(vecbase);
+    set_vecbase(vecbase); // move vec table
 
     main();
 }
 
-/// Move the vector base
+
+/* 
+    We redefine these functions to avoid pulling in xtensa-lx6 as a dependancy
+*/
+
+#[doc(hidden)]
 #[inline]
-pub unsafe fn set_vecbase(base: *const u32) {
-    llvm_asm!("wsr.vecbase $0" ::"r"(base) :: "volatile");
-}
-
-/// Get the core cycle count
-#[inline]
-pub fn get_cycle_count() -> u32 {
-    let x: u32;
-    unsafe { llvm_asm!("rsr.ccount $0" : "=r"(x) ::: "volatile" ) };
-    x
-}
-
-/// Get the core stack pointer
-#[inline(always)]
-pub fn get_stack_pointer() -> *const u32 {
-    let x: *const u32;
-    unsafe { llvm_asm!("mov $0,sp" : "=r"(x) ::: "volatile") };
-    x
-}
-
-/// Set the core stack pointer
-///
-/// *This is highly unsafe!*
-/// It should be used with care at e.g. program start or when building a task scheduler
-///
-/// `stack` pointer to the non-inclusive end of the stack (must be 16-byte aligned)
-#[inline(always)]
-pub unsafe fn set_stack_pointer(stack: *mut u32) {
+unsafe fn set_mask(mut mask: u32) -> u32 {
     llvm_asm!("
-        movi a0,0
-        mov sp,$0
-        " :: "r"(stack):"a0" ::: "volatile" );
+        xsr $0, intenable
+        rsync
+        " :"=r"(mask) :"0"(mask):: "volatile");
+    mask
 }
 
-/// Get the core current program counter
-#[inline(always)]
-pub fn get_program_counter() -> *const u32 {
-    let x: *const u32;
-    let _y: u32;
-    unsafe {
-        llvm_asm!("
-            mov $1,a0
-            call0 1f
-            .align 4
-            1: 
-            mov $0,a0
-            mov a0,$1
-            " : "=r"(x),"=r"(_y)::"a0" : "volatile" )
-    };
-    x
-}
-
-/// cycle accurate delay using the cycle counter register
+#[doc(hidden)]
 #[inline]
-pub fn delay(clocks: u32) {
-    let start = get_cycle_count();
-    loop {
-        if get_cycle_count().wrapping_sub(start) >= clocks {
-            break;
-        }
-    }
-}
-
-/// Get the id of the current core
-#[inline]
-pub fn get_processor_id() -> u32 {
-    let mut x: u32;
-    unsafe { llvm_asm!("rsr.prid $0" : "=r"(x) ::: "volatile") };
-    x
-}
-
-const XDM_OCD_DCR_SET: u32 = 0x10200C;
-const DCR_ENABLEOCD: u32 = 0x01;
-
-/// Returns true if a debugger is attached
-#[inline]
-pub fn is_debugger_attached() -> bool {
-    let mut x: u32;
-    unsafe { llvm_asm!("rer $0,$1" : "=r"(x): "r"(XDM_OCD_DCR_SET) :: "volatile" ) };
-    (x & DCR_ENABLEOCD) != 0
-}
-
-/// Insert debug breakpoint
-#[inline(always)]
-pub fn debug_break() {
-    unsafe { llvm_asm!("break 1,15"::::"volatile") };
+unsafe fn set_vecbase(base: *const u32) {
+    llvm_asm!("wsr.vecbase $0" ::"r"(base) :: "volatile");
 }
